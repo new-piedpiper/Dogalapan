fdefd555b39b18ce7b7eea7ae36d97fd8b5fa760
Anugrah V <59061428+new-piedpiper@users.noreply.github.com>
2022-03-06 12:53:21 +0530

    made changes to random sound selector

--- test.js
+++ test.js
@@ -2,14 +2,11 @@ var a_no=1;
 var ad;
 function groVer()
 {
+  let dat= new Date();
   try {
   ad.pause();
-  } catch (e) {
-
-  } finally {
-
-  }
-  a_no=Math.floor(Math.random()*6)+1;
+  } catch (e) {}
+  a_no=((dat.getSeconds())%6)+1;
   name="ag"+a_no+".mp3";
   ad= new Audio(name);
   ad.play();
